28beedaff97e935b1061fd88d956f3906ff0e386
Andrei SILIN <andrei.silin@studi.fr>
2024-04-15 01:33:16 +0200

    Animations | Navbar

--- src/components/Client/Hero/HeroImage.jsx
+++ src/components/Client/Hero/HeroImage.jsx
@@ -1,8 +1,6 @@
 import React, {useEffect, useRef, useState} from 'react';
 import s from "./HeroImage.module.css";
 import image from "../ui/splash/leaves/vecteezy_ai-genere-bengale-tigre-majestueux-et-feroce-cache-dans_39600637.jpg";
-import Carousel from "../ui/carousel/Carousel";
-import {Button, keyframes} from "@mui/material";
 import HeroNavigation from "../HeroNavigation/HeroNavigation";
 
 const HeroImage = () => {
@@ -32,19 +30,18 @@ const HeroImage = () => {
     return (
         <section className={s.layers} style={{backdropFilter: "saturate(180%)"}}>
 
-            <div className={`${s.navbar} ${showNavbar ? 'open' : ''}`} style={showNavbar ? {left: 0} : {left: 'calc(-50% - 30px)'}}>
-                <div className={s.navbar_close} onClick={() => setShowNavbar(false)}></div>
-                <HeroNavigation/>
+            <div className={`${s.navbar} ${showNavbar ? 'open' : ''}`} style={showNavbar ? {transform: 'translateX(100%)'} : {transform: 'translateX(-50%)'}}>
+                <HeroNavigation handleCloseNavbar={handleOnClick}/>
             </div>
             <div className={`${s.layers__container} ${s.parallax}`}
                  style={{transform: `rotateX(${mousePos.y}deg) rotateY(${mousePos.x}deg)`,}}>
                 <div className={`${s.layers__item} ${s.layer_background}`}
                      style={{backgroundImage: `url(${image})`, filter: "saturate(180%)"}}></div>
                 <div className={`${s.layers__item} ${s.layer_main}`}>
-                    <div className={`${s.main_content}`} style={showNavbar ? {left: '60%'} : {left: '50%'}} >
+                    <div className={`${s.main_content}`} style={showNavbar ? {transform: 'translateX(100%)'} : {transform: 'translateX(0%)'}} >
                         <h1>Arcadia</h1>
                     </div>
-                    <div className={s.description} style={showNavbar ? {left: '60%'} : {left: '50%'}}>Zoo naturel.</div>
+                    <div className={s.description} style={showNavbar ? {transform: 'translateX(100%)'} : {transform: 'translateX(-50%)'}}>Zoo naturel.</div>
                     <a href="#" className={s.button} style={showNavbar ? {opacity: 0} : {opacity: 1}} onClick={handleOnClick}>Commencer l'aventure</a>
                 </div>
             </div>
--- src/components/Client/HeroNavigation/HeroNavigation.jsx
+++ src/components/Client/HeroNavigation/HeroNavigation.jsx
@@ -1,23 +1,82 @@
 import React, {useState} from 'react';
 import s from "./HeroNavigation.module.css";
+import CloseIcon from '@mui/icons-material/Close';
+import MenuIcon from '@mui/icons-material/Menu';
+import image from '../ui/splash/leaves/vecteezy_ai-genere-une-lisse-et-puissant-noir-panthere-rode-par-le_35784039.jpg'
+import image1 from '../ui/splash/leaves/vecteezy_ai-genere-jaguar-haute-qualite-image_38008768.jpg'
+import '@coreui/coreui/dist/css/coreui.min.css'
+import {CCarousel, CCarouselCaption, CCarouselItem, CImage} from "@coreui/react";
 
-const HeroNavigation = () => {
+const HeroNavigation = ({handleCloseNavbar}) => {
 
-    const [activeTab, setActiveTab] = useState('');
+    const [activeTab, setActiveTab] = useState('info');
+    const [hideMenu, setHideMenu] = useState(false)
 
     return (
         <div style={{width: '100%', height: '100%', display: 'flex', gap: 40, position: 'relative'}}>
-            <div className={s.navigation}>
-                <div className={s.navigation__item} onClick={() => setActiveTab('info')}>Info</div>
-                <div className={s.navigation__item} onClick={() => setActiveTab('animals')}>Animaux</div>
-                <div className={s.navigation__item} onClick={() => setActiveTab('habitats')}>Habitats</div>
+            <div className={s.close_icon} onClick={() => handleCloseNavbar()}><CloseIcon/></div>
+            <div className={s.close_icon} onClick={() => setHideMenu(!hideMenu)} style={{marginTop: 40}}><MenuIcon/></div>
+            <div className={`${s.navigation} ${hideMenu ? s.hidden__navigation : s.visible__navigation}`}>
+                <div className={s.navigation__item} style={activeTab === 'info' ? {textDecoration: 'underline'} : {textDecoration: 'none'}} onClick={() => setActiveTab('info')}>Info</div>
+                <div className={s.navigation__item} style={activeTab === 'animals' ? {textDecoration: 'underline'} : {textDecoration: 'none'}} onClick={() => setActiveTab('animals')}>Animaux</div>
+                <div className={s.navigation__item} style={activeTab === 'habitats' ? {textDecoration: 'underline'} : {textDecoration: 'none'}} onClick={() => setActiveTab('habitats')}>Habitats</div>
                 <div className={s.navigation__item}>Avis</div>
             </div>
-            <div style={{position: 'relative', width: '100%'}}>
+            <div style={{position: 'relative', width: '100%', overflow: 'scroll'}}>
                 <div className={s.navigation__content}>
-
-                    <div className={activeTab === 'info' ? s.visible : s.hidden}>
-                        This is info
+                    <div className={`${s.content__item} ${activeTab === 'info' ? s.visible : s.hidden} ${hideMenu ? s.hidden__navigation_content : s.visible__navigation_content}`}>
+                        <div className={s.info__content}>
+                            <div className={`${s.info__item} ${activeTab === 'info' ? s.visible : s.hidden}`}
+                                 style={{marginBottom: 30, fontSize: 32, textAlign: 'center', fontWeight: 'bold'}}>
+                                Bienvenue chez Arcadia
+                            </div>
+                            <div className={`${s.info__item} ${activeTab === 'info' ? s.visible : s.hidden}`}
+                                 style={{maxWidth: 700, marginBottom: 30}}>
+                                Arcadia est une zoo près de la forêt de Brocéliande en Bretagne. Depuis 1960 elle offre
+                                l'occasion unique d'être
+                                le vrai spectateur de la nature, mais serez-vous assez courageux?
+                            </div>
+                            <div style={{marginBottom: 30, alignSelf: 'flex-start'}}>
+                                Nos animaux:
+                            </div>
+                            <CCarousel controls indicators transition="crossfade" style={{width: '100%', paddingLeft: 80, paddingRight: 80, marginBottom: 30}}>
+                                <CCarouselItem interval={1000}>
+                                    <CImage style={{width: '100%', display: 'block', borderRadius: 10}} src={image} alt="slide 1" />
+                                    <CCarouselCaption className="d-none d-md-block">
+                                        <span>Panthéra</span>
+                                    </CCarouselCaption>
+                                </CCarouselItem>
+                                <CCarouselItem  interval={1000}>
+                                    <CImage style={{width: '100%', borderRadius: 10}} src={image1} alt="slide 2" />
+                                    <CCarouselCaption className="d-none d-md-block">
+                                        <span>Léopard</span>
+                                    </CCarouselCaption>
+                                </CCarouselItem>
+                                <CCarouselItem  interval={1000}>
+                                    <CImage style={{width: '100%', borderRadius: 10}} src={image} alt="slide 3" />
+                                </CCarouselItem>
+                            </CCarousel>
+                            <div style={{marginBottom: 30, alignSelf: 'flex-start'}}>
+                                Nos avis:
+                            </div>
+                            <CCarousel transition="crossfade" style={{width: '100%', marginBottom: 30}} pause={false}>
+                                <CCarouselItem interval={1000}>
+                                    <div style={{backgroundColor: 'rgba(0,0,0)', opacity: 1, borderRadius: 20, padding: 20}}>
+                                        Experience incroyable
+                                    </div>
+                                </CCarouselItem>
+                                <CCarouselItem interval={1000}>
+                                    <div style={{backgroundColor: 'rgba(0,0,0)', opacity: 1, borderRadius: 20, padding: 20}}>
+                                        Hello
+                                    </div>
+                                </CCarouselItem>
+                                <CCarouselItem interval={1000}>
+                                    <div style={{backgroundColor: 'rgba(0,0,0)', opacity: 1, borderRadius: 20, padding: 20}}>
+                                        Hello
+                                    </div>
+                                </CCarouselItem>
+                            </CCarousel>
+                        </div>
                     </div>
                     <div className={activeTab === 'animals' ? s.visible : s.hidden}>
                         This is animals
